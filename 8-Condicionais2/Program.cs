87ca7438f125f63985c2c25eda96d631c1e9bccb
Thomas Ravache <thomasravache31@gmail.com>
2022-05-02 23:39:30 -0300

    feat: project 8

---
+++ 8-Condicionais2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Condicionais2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 7 - Condicionais");
+
+            int idadeJoao = 16;
+            int quantidadePessoas = 2;
+
+            bool acompanhado = quantidadePessoas >= 2;
+            // bool acompanhado = true;
+
+            if (idadeJoao >= 18 || acompanhado)
+            {
+                Console.WriteLine("João pode entrar.");
+            }
+            else
+            {
+                Console.WriteLine("João não pode entrar.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
