ef28955935adf04ca5012259cabfefbeecdc858f
Thomas Ravache <thomasravache31@gmail.com>
2022-05-02 23:21:13 -0300

    feat: project 6

---
+++ 6-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_AtribuicoesDeVariaveis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 6");
+
+            int idade = 32;
+            int idadeGustavo = idade;
+
+            idade = 20;
+
+            Console.WriteLine(idade); // 20
+            Console.WriteLine(idadeGustavo); // 32
+
+
+            Console.ReadLine();
+        }
+    }
+}
