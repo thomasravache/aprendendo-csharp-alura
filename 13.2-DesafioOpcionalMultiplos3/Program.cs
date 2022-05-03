a75a125dba67e690779a4fe3f17a4eab5110a441
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-05-03 15:34:20 -0300

    feat: desafioOpcional1

---
+++ 13.2-DesafioOpcionalMultiplos3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13._2_DesafioOpcionalMultiplos3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int numero = 1; numero <= 100; numero++)
+            {
+                if (numero % 3 == 0)
+                {
+                    Console.WriteLine(numero);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
