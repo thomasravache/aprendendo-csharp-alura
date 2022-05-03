bb5de3cea9695043c61a9f10e107f8d66afc06fa
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-05-03 15:48:04 -0300

    feat: DesafioOpcional2

---
+++ 13.3-FatorialOpcional/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13._3_FatorialOpcional
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int numero = 1; numero <= 10; numero++)
+            {
+                int acc = numero;
+                for (int fatorial = numero - 1; fatorial >= 1; fatorial--)
+                {
+                    acc *= fatorial;
+                }
+                Console.WriteLine("Fatorial de " + numero + " é " + acc);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
