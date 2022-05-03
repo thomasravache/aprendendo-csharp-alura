bd87e635a4dccf9b3ad59483b91bbc9a4a5c2e15
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-05-03 15:04:32 -0300

    feat: projects 11, 12, 13

---
+++ 13-ForEncadeado/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_ForEncadeado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto 13");
+
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna < contadorLinha; contadorColuna++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
