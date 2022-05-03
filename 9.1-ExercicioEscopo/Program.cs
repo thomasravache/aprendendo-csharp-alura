770f759f124a957a266168be66b52d5ffe5c26e6
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-05-03 08:53:59 -0300

    feat: exercise 9.1

---
+++ 9.1-ExercicioEscopo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        double salario = 3300.0;
+
+        if (salario >= 1900.0 && salario <= 2800.0)
+        {
+            Console.WriteLine("O IR é de 7.5% e pode deduzir 142,00");
+        }
+
+        if (salario >= 2800.01 && salario <= 3751.0)
+        {
+            Console.WriteLine("O IR é de 15% e pode deduzir 350,00");
+        }
+
+        if (salario >= 3751.01 && salario <= 4664.0) {
+            Console.WriteLine("IR de 222.5% e pode deduzir 636,00");
+        }
+    }
+}
