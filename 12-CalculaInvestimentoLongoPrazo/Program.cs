12c41da99b3aca19449221b05bca6dab549c34dc
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-05-03 14:45:43 -0300

    feat: project 12

---
+++ 12-CalculaInvestimentoLongoPrazo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_CalculaInvestimentoLongoPrazo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 12");
+
+            double fatorRendimento = 1.0036;
+            double valorInvestido = 1000;
+
+            // a cada ano o fator de rendimento aumenta 1%
+
+            for (int contadorAno = 1; contadorAno <=5; contadorAno++)
+            {
+                for (int contadorMes = 1; contadorMes <= 12; contadorMes++)
+                {
+                    valorInvestido *= fatorRendimento;
+                }
+
+                fatorRendimento += 0.0010;
+            }
+
+            Console.WriteLine("Ao término do investimento, você terá R$" + valorInvestido);
+
+            Console.ReadLine();
+        }
+    }
+}
