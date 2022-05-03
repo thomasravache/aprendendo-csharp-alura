6ffa61bacd3b0934db76a982b31eba26ff16f1ec
Thomas Ravache <thomasravache31@gmail.com>
2022-05-02 21:56:11 -0300

    feat: create variables

--- 2-CriandoVariaveis/Program.cs
+++ 2-CriandoVariaveis/Program.cs
@@ -12,6 +12,24 @@ namespace _2_CriandoVariaveis
         {
             Console.WriteLine("Executando projeto 2 - Criando Variaveis");
 
+            int idade;
+            idade = 32;
+
+            Console.WriteLine(idade);
+
+            idade = 10;
+            Console.WriteLine(idade);
+
+            idade = 10 + 5;
+            Console.WriteLine(idade);
+
+            idade = 10 + 5 * 2;
+            Console.WriteLine(idade);
+
+            idade = (10 + 5) * 2;
+            Console.WriteLine(idade);
+            Console.WriteLine("Sua idade é:" + idade + "!");
+
             Console.WriteLine("Execução finalizada. Tecle enter para sair...");
             Console.ReadLine();
         }
